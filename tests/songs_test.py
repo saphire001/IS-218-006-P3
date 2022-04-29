3f5d968060997ab1942e2e0f4fa9c0b842346cb4
saanikajoshi <spj29@njit.edu>
2022-04-29 13:30:45 -0400

    csv test fix hopfully2

--- tests/songs_test.py
+++ tests/songs_test.py
@@ -1,5 +1,8 @@
 
-def test_csv_upload(client):
+
+def test_csv_upload(client, auth):
+    reg = auth.register()
+    log = auth.login()
     csv = "tests/music.csv"
     csv_data = open(csv, "rb")
     data = {"file": (csv_data, "test_music.csv")}
@@ -8,4 +11,4 @@ def test_csv_upload(client):
     print(post.data)
 
     assert post.status_code == 302
-    assert post.headers["Location"] == "/songs_tables"
+    assert post.headers["Location"] == "/songs_tables" 
