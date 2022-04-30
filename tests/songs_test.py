fe2b587c06621739a31e16fa4b8296501b8eb858
saanikajoshi <spj29@njit.edu>
2022-04-30 13:05:02 -0400

    fixed csv upload test working on others

--- tests/songs_test.py
+++ tests/songs_test.py
@@ -1,14 +1,25 @@
+import os
+
+from flask_login import current_user
+
+from app import db
+from app.db.models import User, Song
+
+def test_csv_upload(application):
+    with application.app_context():
+        db.session.query(User).count() == 0
+        db.session.query(Song).count() == 0
+
+        user = User('keith@webizly.com', 'testtest')
+        db.session.add(user)
+
+        user.songs = [Song("T","jgk","2134","jfk"), Song("T2","djf","2342","sdj")]
+        db.session.commit()
+        assert db.session.query(Song).count() == 2
+
+        db.session.delete(user)
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Song).count() == 0
 
 
-def test_csv_upload(client, auth):
-    reg = auth.register()
-    log = auth.login()
-    csv = "tests/music.csv"
-    csv_data = open(csv, "rb")
-    data = {"file": (csv_data, "music.csv")}
-    post = client.post("/songs/upload", data=data)
 
-    print(post.data)
-
-    assert post.status_code == 302
-    assert post.headers["Location"] == "/songs"
